3b106c3f0ff444134a5371c2cf7ebfe6b50b72c3
Joao Ponchiroli <joaopedroponqueroli@gmail.com>
2025-02-22 22:29:43 -0300

    feature: create repository category

---
+++ src/main/java/com/crud/sql/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.crud.sql.repositories;
+
+import com.crud.sql.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
