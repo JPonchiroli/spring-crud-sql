ef0bb767b22b87175b7d9972b285bb889c10aa92
Joao Ponchiroli <joaopedroponqueroli@gmail.com>
2025-02-28 14:26:40 -0300

    feature: add product entity

---
+++ src/main/java/com/crud/sql/entities/Product.java
@@ -0,0 +1,65 @@
+package com.crud.sql.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long product_id;
+    @Column(name = "product_name", nullable = false, length = 100)
+    private String product_name;
+    @Column(name = "product_price", nullable = false)
+    private Double product_price;
+
+    public Product(){}
+
+    public Long getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Long product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(Double product_price) {
+        this.product_price = product_price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(product_id, product.product_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(product_id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product [" +
+                "product_id: " + product_id +
+                ", product_name: " + product_name +
+                ", product_price: " + product_price + "]";
+    }
+}
