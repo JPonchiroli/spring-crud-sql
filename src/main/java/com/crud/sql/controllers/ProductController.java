69d7eecefda40d682525eab6a23ebb3dcab084fa
Joao Ponchiroli <joaopedroponqueroli@gmail.com>
2025-03-10 10:37:44 -0300

    feature: add get all products requisition

--- src/main/java/com/crud/sql/controllers/ProductController.java
+++ src/main/java/com/crud/sql/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.crud.sql.controllers;
 
 import com.crud.sql.dto.ProductDto;
+import com.crud.sql.entities.Category;
 import com.crud.sql.entities.Product;
 import com.crud.sql.mappers.ProductMapper;
 import com.crud.sql.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/products")
@@ -24,5 +25,12 @@ public class ProductController {
 
         service.postProduct(productCreated);
         return ResponseEntity.status(201).body(ProductMapper.toProductDto(productCreated));
+    }
+
+    @GetMapping("get-products")
+    public ResponseEntity<List<Product>> findAllProducts(){
+        List<Product> productList = service.getAllProducts();
+
+        return ResponseEntity.ok().body(productList);
     }
 }
--- src/main/java/com/crud/sql/services/ProductService.java
+++ src/main/java/com/crud/sql/services/ProductService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -15,5 +17,10 @@ public class ProductService {
     @Transactional
     public Product postProduct(Product product){
         return repository.save(product);
+    }
+
+    @Transactional
+    public List<Product> getAllProducts(){
+        return repository.findAll();
     }
 }
